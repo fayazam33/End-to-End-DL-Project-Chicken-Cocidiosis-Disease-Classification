931970c10893f26976ac6c58ea6413ce6381c864
Fayaz Ali Muktadir <121030621+fayazam33@users.noreply.github.com>
2026-09-07 23:57:56 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # End-to-End-DL-Project-Chicken-Cocidiosis-Disease-Classification
+
+# How to run?
+### STEPS:
+
+Clone the repository
+
+```
+https://github.com/entbappy/End-to-End-Deep-Learning-Project-Chicken-Disease
+```
+### STEP 01- Create a conda environment after opening the repository
+```bash
+conda create -n chicken python=3.8 -y
+```
+```bash
+conda activate chicken
+```
+### STEP 02- install the requirements
+```bash
+pip install -r requirements.txt
+```
+Export the environment
+export AWS_ACCESS_KEY_ID="YOUR_ACCESS_KEY_ID""
+export AWS_SECRET_ACCESS_KEY="YOUR_SECRET_ACCESS_KEY"
+# Finally run the following command
+python app.py
+Now,
+
+open up you local host and port
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-DL-Project-Chicken-Cocidiosis-Disease-Classification"
+AUTHOR_USER_NAME = "fayazam33" #github user name 
+SRC_REPO = "chicken_disease_classifier"
+AUTHOR_EMAIL = "456problemthinker@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+#setup.py ins future problem pyproject.toml
